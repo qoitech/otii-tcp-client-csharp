03f5f593559b91076aad08b1530930da82fd5b60
Joakim Kvarnlöf <joakim@kvarnlof.com>
2021-10-27 10:22:55 +0200

    Added basic measurement example

---
+++ Examples/BasicMeasurement/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using Otii;
+
+namespace BasicMeasurement {
+    class Program {
+        private const int RecordingTime = 30000;
+
+        static void Main(string[] args) {
+            // Calling Connect without parameters will connect to a local instance of Otii
+            var client = new OtiiClient();
+            client.Connect();
+
+            // Create a local reference to the Otii property for convenience
+            var otii = client.Otii;
+
+            // Get a list of all Otii devices available
+            var devices = otii.GetDevices();
+            if (devices.Length == 0) {
+                throw new Exception("No available devices");
+            }
+
+            // Get a reference to the first device in the list
+            var arc = devices[0];
+
+            var project = otii.GetActiveProject();
+            if (project == null) {
+                project = otii.CreateProject();
+            }
+
+            // Configuration
+            arc.SetMainVoltage(3.3);
+            arc.SetMaxCurrent(0.5);
+            arc.EnableUart(true);
+            arc.SetUartBaudrate(115200);
+            arc.EnableChannel("mc", true);
+            arc.EnableChannel("mv", true);
+            arc.EnableChannel("rx", true);
+            arc.EnableChannel("i1", true);
+
+            // Record
+            project.StartRecording();
+            arc.SetMain(true);
+            Thread.Sleep(RecordingTime);
+            arc.SetMain(false);
+            project.StopRecording();
+
+            // Close the connection
+            client.Close();
+        }
+    }
+}
